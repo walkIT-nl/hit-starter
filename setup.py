696f8f30c0f31c57dae22628652cccd05590fd99
Walco van Loon <walco@walkit.nl>
2021-01-19 16:30:05 +0100

    Fix requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['requests', 'fhir-resources==6.0.0' ]
+requirements = ['requests', 'dataclasses', 'fhir.resources==6.0.0' ]
 
 setup_requirements = ['pytest-runner', ]
 
